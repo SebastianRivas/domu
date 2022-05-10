32d251675f6885f52217518a049e15bcf7837c14
jsr1v4s <jsrivas@superfuds.com.co>
2022-05-09 20:40:58 -0500

    update web.php file

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ use Inertia\Inertia;
 |
 */
 
+if (env('APP_ENV') === 'production') {
+    URL::forceSchema('https');
+}
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
